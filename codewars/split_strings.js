b8d3ac0418033d8153e7f5d7ebbedeb10e0ccf65
Guilherme Noronha <163042193+guinoronhaf@users.noreply.github.com>
2024-04-17 22:06:45 -0300

    codewars

---
+++ codewars/split_strings.js
@@ -0,0 +1,13 @@
+function solution(str) {
+    let strSplit = str.split('')
+    let array = []
+    if (strSplit.length % 2 != 0) {
+        strSplit.push('_')
+    }
+    for (let i = 0; i < strSplit.length; i +=2) {
+        array.push(`${strSplit[i]}${strSplit[i + 1]}`)
+    }
+    return array
+}
+
+console.log(solution('abc'))
