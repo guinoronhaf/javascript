4da36898583fb44cc09bd617646e2108fc63b952
Guilherme Noronha <163042193+guinoronhaf@users.noreply.github.com>
2024-04-25 19:19:05 -0300

    aulas e mais aulas

--- curso_completasso/aula79/script.js
+++ curso_completasso/aula79/script.js
@@ -1,5 +1,44 @@
 const div_data = document.querySelector('#div_data')
 const div_relogio = document.querySelector('#div_relogio')
+// const som_alarme = document.querySelector('#som_alarme')
+const btn_ativar = document.querySelector('#btn_ativar')
+const btn_parar = document.querySelector('#btn_parar')
+const tpm_alarme = document.querySelector('#tmp_alarme')
+const hora_alarme = document.querySelector('#hora_alarme')
+const timer = document.querySelector('#timer')
+
+const som_alarme = new Audio()
+som_alarme.loop = -1 //tocar para sempre
+
+
+let ts_atual = null
+let ts_alarme = null
+let alarme_ativado = false
+let alarme_tocando = false
+
+btn_ativar.addEventListener("click", () => {
+    ts_atual = Date.now()
+    ts_alarme = ts_atual + (tpm_alarme.value * 1000)
+    alarme_ativado = true
+    const dt_alarme = new Date(ts_alarme) // data que quer que o alarme toque
+    let alarme_hora = dt_alarme.getHours()
+    alarme_hora = alarme_hora<10?'0'+alarme_hora:alarme_hora
+    let alarme_minuto = dt_alarme.getMinutes()
+    alarme_minuto = alarme_minuto<10?'0'+alarme_minuto:alarme_minuto
+    let alarme_segundo = dt_alarme.getSeconds()
+    alarme_segundo = alarme_segundo<10?'0'+alarme_segundo:alarme_segundo
+    hora_alarme.innerHTML = `Hora do alarme: ${alarme_hora}:${alarme_minuto}:${alarme_segundo} </br>`
+})
+
+btn_parar.addEventListener("click", () => {
+    alarme_ativado = false
+    alarme_tocando = false
+    hora_alarme.innerHTML = 'Hora do alarme:'
+    tpm_alarme.value = 0
+    timer.classList.remove('alarme')
+    som_alarme.pause()
+    som_alarme.currentTime = 0
+})
 
 const data = new Date()
 
@@ -18,7 +57,14 @@ const relogio = () => {
     let segundo = data.getSeconds()
     segundo = segundo<10?'0'+segundo:segundo
     const hora_completa = hora+':'+minuto+':'+segundo
-    div_relogio.innerHTML =hora_completa
+    div_relogio.innerHTML = hora_completa
+    if (alarme_ativado && !alarme_tocando) {
+        if (data.getTime() >= ts_alarme) {
+            alarme_tocando = true
+            som_alarme.play()
+            timer.classList.add('alarme')
+        }
+    }
 }
 
 const intervalo = setInterval(relogio, 1000)
