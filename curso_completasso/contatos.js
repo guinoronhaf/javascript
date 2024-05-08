53c517c69f4b2c9d3cf54cbcda2ec7de65490386
Guilherme Noronha <163042193+guinoronhaf@users.noreply.github.com>
2024-05-07 21:29:03 -0300

    peso

--- curso_completasso/contatos.js
+++ curso_completasso/contatos.js
@@ -1,15 +1,15 @@
 import { contatos } from './bancoContatos.js'
 
-let btnApagarArray = []
+// let btnApagarArray = []
 
-const criarBtnApagar = () => {
-    const btnApagar = document.createElement('div')
-    btnApagar.setAttribute('style', 'display: flex; justify-content:center; align-items: center; width: 30px; height: 30px; background-color: red; color: white; border-radius: 50%; cursor: pointer; align-self: center')
-    btnApagar.innerHTML = '<i class="bi bi-trash3"></i>'
-    btnApagar.setAttribute('class', 'btnApagar')
-    btnApagarArray.push(btnApagar)
-    return btnApagar
-}
+// const criarBtnApagar = () => {
+//     const btnApagar = document.createElement('div')
+//     btnApagar.setAttribute('style', 'display: flex; justify-content:center; align-items: center; width: 30px; height: 30px; background-color: red; color: white; border-radius: 50%; cursor: pointer; align-self: center')
+//     btnApagar.innerHTML = '<i class="bi bi-trash3"></i>'
+//     btnApagar.setAttribute('class', 'btnApagar')
+//     btnApagarArray.push(btnApagar)
+//     return btnApagar
+// }
 
 let contato = {
     getTodosContatos: function() {
@@ -27,29 +27,44 @@ let contato = {
         contatos.push(cont)
         destinoDOM.innerHTML = ''
         contatos.forEach((el, pos) => {
+            const btn = document.createElement('button')
+            btn.setAttribute('id', 'btn_' + pos)
+            btn.setAttribute('style', 'background-color: red; color: white')
             const div = document.createElement('div')
             div.setAttribute('class', 'contato')
-            const p_nome = document.createElement('p')
-            const p_telefone = document.createElement('p')
-            const p_email = document.createElement('p')
-            p_nome.innerHTML = el.nome
-            p_telefone.innerHTML = el.telefone
-            p_email.innerHTML = el.email
-            div.appendChild(p_nome)
-            div.appendChild(p_telefone)
-            div.appendChild(p_email)
-            const btn = criarBtnApagar()
-            div.appendChild(btn)
-            destinoDOM.appendChild(div)
-            div.lastChild.addEventListener("click", (evt) => {
-                let c = this.getTodosContatos()
-                // console.log(c[pos])
-                c.splice(pos, 1)
-                console.log(c)
+            btn.innerHTML = 'Remover'
+            btn.addEventListener("click", () => {
+                destinoDOM.innerHTML = ''
+                contatos.splice(pos, 1)
+                contato.addContato(contatos, destinoDOM)
             })
 
-            // this.apagarCadaBox(div, this.getTodosContatos())
-
+            if (el.nome != undefined) {
+                const p_nome = document.createElement('p')
+                const p_telefone = document.createElement('p')
+                const p_email = document.createElement('p')
+                p_nome.innerHTML = el.nome
+                p_telefone.innerHTML = el.telefone
+                p_email.innerHTML = el.email
+                div.appendChild(p_nome)
+                div.appendChild(p_telefone)
+                div.appendChild(p_email)
+                div.appendChild(btn)
+                destinoDOM.appendChild(div)
+            } else {
+                contatos.pop()
+            }
+            // const btn = criarBtnApagar()
+            // div.appendChild(btn)
+            // div.lastChild.addEventListener("click", () => {
+            //     contatos.splice(pos, 1, '')
+            //     contatos[pos].nome = ''
+            //     contatos[pos].telefone = ''
+            //     contatos[pos].email = ''
+            //     btn.style.display = 'none'
+            //     console.log(contatos)
+            // })
+            // destinoDOM.appendChild(div)
         })
         // const p_nome = document.createElement('p')
         // p_nome.innerHTML = novoContato.nome
@@ -61,10 +76,10 @@ let contato = {
         // div.appendChild(p_telefone)
         // div.appendChild(p_email)
         // destinoDOM.appendChild(div)
-    },
-    getTodosbtn: function() {
-        return btnApagarArray
-    }, 
+    }
+    // getTodosbtn: function() {
+    //     return btnApagarArray
+    // }
 }
 
 export default contato
