779ab608977c9cd4196c391edfd5bad77255a829
Guilherme Noronha <163042193+guinoronhaf@users.noreply.github.com>
2024-05-10 21:37:46 -0300

    estudando

---
+++ codewars/break_camel_case/script.js
@@ -0,0 +1,19 @@
+// let s = 'camelCasing'
+// let sSpl = s.split('')
+// let regex = /[A-Z]?/
+// const pos = s.match(regex).index
+// sSpl.splice(pos, 0, ' ')
+// console.log(sSpl.join(''))
+
+// function solution(string) {
+//     let strSpl = string.split('')
+//     if (string.match(/[A-Z]/g)) {
+//         const pos = string.match(/[A-Z]/g).index
+//         strSpl.splice(pos, 0, ' ')
+//         return strSpl.join('')
+//     } else {
+//         return string
+//     }
+// }
+
+// console.log(solution('camelCasingTest'))
--- codewars/build_tower/script.js
+++ codewars/build_tower/script.js
@@ -1,13 +1,10 @@
 function towerBuilder(nFloors) {
     let result = []
-    let numberOfSymbols = []
-    for (let n = 1; n < nFloors * 2; n += 2) {
-        numberOfSymbols.push(n)
-    }
     for (let i = 1; i <= nFloors; i++) {
-        result.push(`${' '.repeat(nFloors - i)}${'*'.repeat(numberOfSymbols[i - 1])}${' '.repeat(nFloors - i)}`)
+        result.push(`${' '.repeat(nFloors - i)}${'*'.repeat(i + i - 1)}${' '.repeat(nFloors - i)}`)
     }
     return result
 }
+
 
 console.log(towerBuilder(5))
