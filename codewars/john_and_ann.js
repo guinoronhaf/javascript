2ec06cc5c2a9c219dda2627381807e9522659cd6
Guilherme Noronha <163042193+guinoronhaf@users.noreply.github.com>
2024-06-01 21:47:11 -0300

    trying

---
+++ codewars/john_and_ann.js
@@ -0,0 +1,25 @@
+function ann(n) {
+    
+    let katasAnn = [1];
+
+    return katasAnn;
+
+};
+
+function john(n) {
+
+    let katasJohn = [0];
+
+    for (let i = 1; i < n; i++) {
+        katasJohn.push()
+    };
+
+};
+
+function sum_ann(n) {
+
+};
+
+function sum_john(n) {
+
+};
---
+++ codewars/not_very_secure.js
@@ -0,0 +1,5 @@
+const str = "1!1KaOpMm0D34QPn";
+
+const regex = /^[a-z0-9]+$/i
+
+console.log(regex.test(str))
