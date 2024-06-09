a7167cd3b12033462f3aa0f220ccd2108eb0c2ae
Guilherme Noronha <163042193+guinoronhaf@users.noreply.github.com>
2024-06-09 20:28:03 -0300

    pangram task

---
+++ exercism.io/pangram.js
@@ -0,0 +1,15 @@
+const isPangram = (word) => {
+    if (!word) return false;
+    const onlyLetters = word.match(/[a-z]/ig);
+    let aux = [], count = 0;
+    onlyLetters.map((letter) => {
+      const validation = new RegExp(letter, 'i');
+      if (!validation.test(aux.join(''))) {
+        aux.push(letter);
+        count++;
+      }
+    });
+    return count >= 26?true:false;
+};
+
+console.log(isPangram('five boxing wizards jump quickly at it'))
