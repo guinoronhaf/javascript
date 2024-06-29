1af015002669a54ad82dbcbc60ea8823fc3ec005
Guilherme Noronha <163042193+guinoronhaf@users.noreply.github.com>
2024-06-28 21:05:31 -0300

    app estoque -> 3

--- curso_completasso/aula182/colaboradores/colaboradores.js
+++ curso_completasso/aula182/colaboradores/colaboradores.js
@@ -1,5 +1,6 @@
 const dadosGrid = document.querySelector('#dadosGrid');
 const btnAdd = document.querySelector('.btn_mainoper');
+const tituloPopup = document.querySelector('#tituloPopup');
 const novoColaborador = document.querySelector('#novoColaborador');
 const btnFechar = document.querySelector('#btn_fechar');
 const btnGravar = document.querySelector('#btn_gravar');
@@ -11,43 +12,118 @@ const telefones = document.querySelector('#telefones');
 const fTel = document.querySelector('#f_tel');
 const fFoto = document.querySelector('#f_foto');
 const imgFoto = document.querySelector('#img_foto');
-const endpoint_todosColaboradores = `http://127.0.0.1:1880/todosusuarios`;
 
-fetch(endpoint_todosColaboradores)
-.then(res => res.json()) 
-.then(data => {
-    dadosGrid.innerHTML = '';
-    data.forEach((e) => {
-        const linhaGrid = document.createElement('div');
-        linhaGrid.setAttribute("class", "linhaGrid");
+let modoJanela = "n"; // n => novo colaborador; e => editar
+let currentId = null;
 
-        const c1 = document.createElement('div');
-        c1.setAttribute("class", "colunaLinhaGrid c1");
-        c1.textContent = e.n_usuario_usuario;
-        linhaGrid.appendChild(c1);
+const criarCxTelefone = num => {
+    const tel = document.createElement('div');
+    tel.setAttribute("class", "tel");
 
-        const c2 = document.createElement('div');
-        c2.setAttribute("class", "colunaLinhaGrid c2");
-        c2.textContent = e.s_nome_usuario;
-        linhaGrid.appendChild(c2);
+    const numTel = document.createElement('div');
+    numTel.setAttribute("class", "numTel");
+    numTel.textContent = num;
 
-        const c3 = document.createElement('div');
-        c3.setAttribute("class", "colunaLinhaGrid c3");
-        c3.textContent = e.n_tipousuario_tipousuario;
-        linhaGrid.appendChild(c3);
-
-        const c4 = document.createElement('div');
-        c4.setAttribute("class", "colunaLinhaGrid c4");
-        c4.textContent = e.c_status_usuario;
-        linhaGrid.appendChild(c4);
-
-        const c5 = document.createElement('div');
-        c5.setAttribute("class", "colunaLinhaGrid c5");
-        linhaGrid.appendChild(c5);
-
-        dadosGrid.appendChild(linhaGrid);
+    const delTel = document.createElement('img');
+    delTel.setAttribute("class", "delTel");
+    delTel.setAttribute("src", "../imgs/delete.svg");
+    delTel.setAttribute("alt", "Deletar número");
+    delTel.setAttribute("title", "Deletar número");
+    delTel.addEventListener("click", (e) => {
+        e.target.parentNode.remove();
     });
-});
+
+    tel.appendChild(numTel);
+    tel.appendChild(delTel);
+
+    telefones.appendChild(tel);
+};
+
+const mostrarDadosGrid = () => {
+    const endpoint_todosColaboradores = `http://127.0.0.1:1880/todosusuarios`;
+    fetch(endpoint_todosColaboradores)
+    .then(res => res.json()) 
+    .then(data => {
+        dadosGrid.innerHTML = '';
+        data.forEach((e) => {
+            const linhaGrid = document.createElement('div');
+            linhaGrid.setAttribute("class", "linhaGrid");
+    
+            const c1 = document.createElement('div');
+            c1.setAttribute("class", "colunaLinhaGrid c1");
+            c1.textContent = e.n_usuario_usuario;
+            linhaGrid.appendChild(c1);
+    
+            const c2 = document.createElement('div');
+            c2.setAttribute("class", "colunaLinhaGrid c2");
+            c2.textContent = e.s_nome_usuario;
+            linhaGrid.appendChild(c2);
+    
+            const c3 = document.createElement('div');
+            c3.setAttribute("class", "colunaLinhaGrid c3");
+            c3.textContent = e.n_tipousuario_tipousuario;
+            linhaGrid.appendChild(c3);
+    
+            const c4 = document.createElement('div');
+            c4.setAttribute("class", "colunaLinhaGrid c4");
+            c4.textContent = e.c_status_usuario;
+            linhaGrid.appendChild(c4);
+    
+            const c5 = document.createElement('div');
+            c5.setAttribute("class", "colunaLinhaGrid c5");
+            linhaGrid.appendChild(c5);
+    
+            const imgStatus = document.createElement('img');
+            imgStatus.setAttribute("src", "../imgs/on.svg");
+            c5.appendChild(imgStatus);
+    
+            const imgEdit= document.createElement('img');
+            imgEdit.setAttribute("src", "../imgs/edit.svg");
+            imgEdit.setAttribute("id", "btnEdit");
+            imgEdit.addEventListener("click", (e) => {
+                fNome.value = "";
+                fTipoColab.value = "";
+                fStatus.value = "";
+                fFoto.value = "";
+                imgFoto.setAttribute("src", "#");
+                telefones.innerHTML = "";
+                tituloPopup.innerHTML = 'Editar colaborador(a)';
+                modoJanela = "e";
+                const id = e.target.parentNode.parentNode.firstChild.innerHTML;
+                currentId = id;
+                let endpoint = `http://127.0.0.1:1880/todosusuarios/${id}`;
+                fetch(endpoint)
+                .then(res => res.json())
+                .then(data => {
+                    fNome.value = data[0].s_nome_usuario;
+                    fTipoColab.value = data[0].n_tipousuario_tipousuario;
+                    fStatus.value = data[0].c_status_usuario;
+                    imgFoto.setAttribute("src", data[0].s_foto_usuario);
+                    novoColaborador.classList.remove('ocultarPopup');
+                })
+    
+                endpoint = `http://127.0.0.1:1880/telefone/${id}`;
+                fetch(endpoint)
+                .then(res => res.json())
+                .then(data => {
+                    data.forEach(d => {
+                        criarCxTelefone(d.s_numero_telefone);
+                    })
+                })
+            });
+            c5.appendChild(imgEdit);
+    
+            const imgDelete = document.createElement('img');
+            imgDelete.setAttribute("src", "../imgs/delete.svg");
+            imgEdit.setAttribute("id", "btnDelete");
+            c5.appendChild(imgDelete);
+    
+            dadosGrid.appendChild(linhaGrid);
+        })
+    });
+};
+
+mostrarDadosGrid();
 
 const endpoint_tiposColab = `http://127.0.0.1:1880/tiposcolab`;
 fetch(endpoint_tiposColab)
@@ -63,6 +139,14 @@ fetch(endpoint_tiposColab)
 })
 
 btnAdd.addEventListener("click", () => {
+    modoJanela = "n";
+    fNome.value = "";
+    fTipoColab.value = "";
+    fStatus.value = "";
+    fFoto.value = "";
+    imgFoto.setAttribute("src", "#");
+    telefones.innerHTML = "";
+    tituloPopup.innerHTML = 'Novo(a) colaborador(a)';
     novoColaborador.classList.remove('ocultarPopup');
 });
 
@@ -71,38 +155,58 @@ btnFechar.addEventListener("click", () => {
 });
 
 btnGravar.addEventListener("click", () => {
+    if (modoJanela == "n") {
+        const tels = [...document.querySelectorAll('.numTel')];
+        let numTels = [];
 
-    const tels = [...document.querySelectorAll('.numTel')];
-    let numTels = [];
+        tels.forEach(tel => {
+            numTels.push(tel.textContent);
+        });
 
-    tels.forEach(tel => {
-        numTels.push(tel.textContent);
-    });
+        const dados = {
+            s_nome_usuario:fNome.value,
+            n_tipousuario_tipousuario:fTipoColab.value,
+            c_status_usuario:fStatus.value,
+            numTelefones:numTels,
+            s_foto_usuario: imgFoto.getAttribute("src")
+        };
 
-    const dados = {
-        s_nome_usuario:fNome.value,
-        n_tipousuario_tipousuario:fTipoColab.value,
-        c_status_usuario:fStatus.value,
-        numTelefones:numTels,
-        s_foto_usuario: imgFoto.getAttribute("src")
-    };
+        const cabecalho = {
+            method: 'POST',
+            body: JSON.stringify(dados)
+        };
+        const endpoint_novoColab = `http://127.0.0.1:1880/novocolab`;
+        fetch(endpoint_novoColab, cabecalho)
+        .then(res => {
+            if (res.status == 200) {
+                alert('Novo usuário cadastrado');
+                fNome.value = '';
+                fTipoColab.value = '';
+                fStatus.value = '';
+                fFoto.value = '';
+                imgFoto.src = '#';
+                telefones.innerHTML = "";
+            } else {
+                alert('Erro ao cadastrar usuário');
+            }
+        })
 
-    const cabecalho = {
-        method: 'POST',
-        body: JSON.stringify(dados)
-    };
-    const endpoint_novoColab = `http://127.0.0.1:1880/novocolab`;
-    fetch(endpoint_novoColab, cabecalho)
-    .then(res => {
-        if (res.status == 200) {
-            alert('Novo usuário cadastrado');
-        } else {
-            alert('Erro ao cadastrar usuário');
-        }
-    })
+        novoColaborador.classList.add('ocultarPopup');
+    } else if (modoJanela == "e") {
+        let endpointDelete = `http://127.0.0.1:1880/deletartelefones/${currentId}`;
+        fetch(endpointDelete, {
+            method: 'DELETE'
+        })
+        .then(res => {
+            if (res.status == 200) {
+                console.log('deletado');
+            } else {
+                console.log('erro');
+            }
+        })
 
-    novoColaborador.classList.add('ocultarPopup');
-
+        // let endpointUpdate = 
+    }
 });
 
 btnCancelar.addEventListener("click", () => {
@@ -114,30 +218,9 @@ fTel.addEventListener("keyup", (e) => { //se fosse assim que apertasse a tecla, 
         const regex = /[\d]{11}/;
 
         if (regex.test(e.target.value)) {
-            const tel = document.createElement('div');
-            tel.setAttribute("class", "tel");
-
-            const numTel = document.createElement('div');
-            numTel.setAttribute("class", "numTel");
-            numTel.textContent = e.target.value;
-
-            const delTel = document.createElement('img');
-            delTel.setAttribute("class", "delTel");
-            delTel.setAttribute("src", "../imgs/delete.svg");
-            delTel.setAttribute("alt", "Deletar número");
-            delTel.setAttribute("title", "Deletar número");
-            delTel.addEventListener("click", (e) => {
-                e.target.parentNode.remove();
-            });
-
-            tel.appendChild(numTel);
-            tel.appendChild(delTel);
-
-            telefones.appendChild(tel);
-
+            criarCxTelefone(e.target.value);
             e.target.value = '';
             e.target.focus();
-
         } else {
             alert('Numero de telefone inválido');
         }
